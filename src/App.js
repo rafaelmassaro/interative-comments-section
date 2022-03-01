4bd32934464a3d001a2739d6d9a443f36e530423
Rafael <rafaelmassaro9@gmail.com>
2022-03-01 17:30:48 -0300

    Add nested comments

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
+import { useEffect,  useState} from "react";
+
 import MessageBox from "./components/MessageBox";
-import Form from './components/Form';
+
+import { api } from './services/api'
+
 
 function App() {
+  const [data, setData] = useState([])
+
+    useEffect(() => {
+        const loadComments = async () => {
+            const response = await api.get('comments')
+
+            setData(response.data)
+        }
+
+        loadComments()
+    }, [])
+
   return (
     <div className="App">
-      <MessageBox />
-
-      <Form />
+      <ul>
+      {data.map((item) => (
+        <li key={item.id}>
+          <MessageBox comment={item} />
+        </li>
+      ))}
+      </ul>
+      
     </div>
   );
 }
--- src/components/MessageBox/index.js
+++ src/components/MessageBox/index.js
@@ -1,5 +1,3 @@
-import { useState, useEffect } from 'react'
-
 import './messagebox.css';
 
 import IconEdit from '../../assets/images/icon-edit.svg';
@@ -8,73 +6,68 @@ import IconReply from '../../assets/images/icon-reply.svg';
 
 import VoteButton from '../VoteButton';
 import FormButton from '../FormButton';
+import Form from '../Form';
 
-import {api} from '../../services/api';
 
-
-export default function MessageBox() {
-    const [comments, setComments] = useState([])
-
-    useEffect(() => {
-        const loadComments = async () => {
-            const response = await api.get('comments')
-
-            setComments(response.data)
-        }
-
-        loadComments()
-    }, [])
+export default function MessageBox({comment}) {
+    
 
     return(
-        <ul>
-            {comments.map(comment => (
-                <li key={comment.id}>
-                    <div className="container">
-                        <section className="vote-area">
-                            <VoteButton score={comment.score} />
-                        </section>
-                        <section className="message">
-                            <div className="user-area">
-                                <div className="user-info">
-                                    <div className="user-avatar">
-                                        <img src={comment.user.image.webp} alt={comment.user.username} />          
-                                    </div>
-
-                                    <a className='user-name' href="#">{comment.user.username}</a>
-
-                                    {/* <span className='user-mark'>
-                                        you
-                                    </span> */}
-
-                                    <span className='message-date'>{comment.createdAt}</span>
-                                </div>
-
-                                <div className='container-buttons'>
-                                    {/* <button className='delete-button'>
-                                        <img src={IconDelete} alt="Icon Delete"/>
-                                        Delete
-                                    </button>
-                                    <button className='edit-button'>
-                                        <img src={IconEdit} alt="Icon Edit"/>
-                                        Edit
-                                    </button> */}
-                                    <button className='edit-button'>
-                                        <img src={IconReply} alt="Icon Reply"/>
-                                        Reply
-                                    </button>
-                                </div>
-
+        <>
+            <div className="container">
+                <section className="vote-area">
+                    <VoteButton score={comment.score} />
+                </section>
+                <section className="message">
+                    <div className="user-area">
+                        <div className="user-info">
+                            <div className="user-avatar">
+                                <img src={comment.user.image.webp} alt={comment.user.username} />          
                             </div>
-                            <p className="message-content">
-                            {comment.content}
-                            </p>
-                        </section>
+
+                                <a className='user-name' href="#">{comment.user.username}</a>
+
+                                <span className='user-mark'>
+                                    you
+                                </span>
+
+                                <span className='message-date'>{comment.createdAt}</span>
+                        </div>
+
+                        <div className='container-buttons'>
+                            {/* <button className='delete-button'>
+                                <img src={IconDelete} alt="Icon Delete"/>
+                                    Delete
+                            </button>
+                            <button className='edit-button'>
+                                <img src={IconEdit} alt="Icon Edit"/>
+                                    Edit
+                            </button> */}
+                            <button className='edit-button'>
+                                <img src={IconReply} alt="Icon Reply"/>
+                                    Reply
+                            </button>
+                        </div>
+
                     </div>
+                        <p className="message-content">
+                            {comment.content}
+                        </p>
+                        
+                </section>
+            </div>
+            <div>
+            {comment.replies && (
+                <ul className='replies-list'>
+                    {comment.replies.map(reply => (
+                        <li className='reply' key={reply.id}>
+                            <MessageBox comment={reply}/>
+                        </li>
+                    ))}
+                </ul>
+            )}
+            </div>
+        </>
 
-
-
-                </li>
-            ))}
-        </ul>
     )
 }
